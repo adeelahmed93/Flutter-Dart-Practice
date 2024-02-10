ce0e15978ac6d97dc1df7273584da4b643ebce92
adeelahmed93 <a.a.albalushi0342@gmail.com>
2024-02-10 20:10:17 +0500

    again updated even_odd.dart

--- even_odd.dart
+++ even_odd.dart
@@ -7,11 +7,17 @@ void main() {
   stdout.write("Write a Number to know Even or Odd: ");
   int? number = int.parse(stdin.readLineSync()!);
 
-  if (number.isEven && number % 5 == 0) {
-    print("$number is an even number & Divisible by  5");
-  } else if (number.isOdd && number % 7 == 0) {
-    print("$number is an odd number & Divisible by 7");
+  if (number % 2 == 0) {
+    if (number % 5 == 0) {
+      print("$number is an even and Divisible by  5");
+    } else {
+      print("$number is an even but not divisible by 5");
+    }
   } else {
-    print("$number is an odd numebr it isn't divisible 5 & 7");
+    if (number % 7 == 0) {
+      print("$number is an odd and Divisible by 7");
+    } else {
+      print("$number is an odd but not Divisible by 7");
+    }
   }
-  }
+}
